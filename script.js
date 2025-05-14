ccfec270c280a19fb5287b138ff858a9a7f6888d
¨Macarena <videlamacarenalourdes@gmail.com>
2025-05-14 14:03:24 -0300

    cambios

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-  var timelineItems = document.querySelectorAll(".timeline-item");
-  timelineItems.forEach(function(item) {
-    var position = item.getBoundingClientRect();
-    if (position.top < window.innerHeight && position.bottom >= 0) {
-      item.classList.add("show");
-    }
-  });
